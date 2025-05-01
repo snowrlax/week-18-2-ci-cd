6275748f66f8bd525dc37e27eabed428efd4c972
snowrlax <sonawanepranav19@gmail.com>
2025-05-01 21:37:09 +0530

    another new changes

--- apps/merchant-app/app/page.tsx
+++ apps/merchant-app/app/page.tsx
@@ -2,13 +2,11 @@
 
 import { useBalance } from "@repo/store/balance";
 
-export default function() {
+export default function () {
   const balance = useBalance();
-  return <div>
-<<<<<<< HEAD
-    Hello there your balance iss :  {balance}
-=======
-    hi there {balance}
->>>>>>> bf8fcd4de2763af1ac0fcb2c0364b4c163259cf6
+  return (
+  <div>
+    Hello there your balance iss : {balance}
   </div>
+  )
 }
